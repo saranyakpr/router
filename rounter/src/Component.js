6698a565c93254e41d0a725c24487b53cf7393e8
saranyakpr <saranjokpr@gmail.com>
2023-01-15 22:05:22 -0500

    router

---
+++ rounter/src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Component.scss'
+
+const Component = () => {
+  return (
+    <div className='container'>
+       Component
+    </div>
+);
+}
+
+export default Component
